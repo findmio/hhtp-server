fe0d4d610eff16827dffd4311981a96ea6d85566
findmio <sharida@163.com>
2022-03-03 07:41:30 +0800

    fix: 修复版本号不能打印的 bug

--- bin/options.js
+++ bin/options.js
@@ -1,7 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-const pkg = JSON.parse(fs.readFileSync(path.resolve('./package.json'), 'utf-8'));
-
 export default {
   'port': {
     option: '-p,--port <number>',
@@ -12,7 +8,6 @@ export default {
   'version': {
     option: '-v --version',
     description: 'Print the version and exit.',
-    default: pkg.version,
     usage: 'http-server --version'
   },
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import fs from 'fs';
 import chalk from 'chalk';
+import path from 'path';
+const pkg = JSON.parse(fs.readFileSync(path.resolve(process.argv[1], '../../package.json'), 'utf-8'));
 
 export default class HttpServer {
   constructor(options) {
-    console.log(options);
+    this.version = options.version;
+    this.port = options.port;
   }
   createServer() {
+    if (this.version) {
+      console.log(pkg.version);
+      return;
+    }
+    console.log(process.cwd());
     console.log(chalk.yellow('Hit CTRL-C to stop the server'));
   }
 }
